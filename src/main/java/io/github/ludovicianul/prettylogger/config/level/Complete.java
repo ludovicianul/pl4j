240f11c9da8861beee72b670eed0d1683d8b66eb
Madalin Ilie <iliemad@gmail.com>
2020-12-05 20:30:07 +0200

    Change symbol for complete

--- src/main/java/io/github/ludovicianul/prettylogger/config/level/Complete.java
+++ src/main/java/io/github/ludovicianul/prettylogger/config/level/Complete.java
@@ -1,12 +1,12 @@
 package io.github.ludovicianul.prettylogger.config.level;
 
-import io.github.ludovicianul.prettylogger.config.PrettyConfig;
+import io.github.ludovicianul.prettylogger.config.PrettyMarker;
 import org.fusesource.jansi.Ansi;
 import org.slf4j.event.Level;
 
-class Complete extends PrettyConfig {
+class Complete extends PrettyMarker {
     private Complete() {
-        super("complete", "☒", Ansi.Color.CYAN, Level.INFO);
+        super("complete", "\u2611", Ansi.Color.CYAN, Level.INFO);
     }
 
     static Complete defaultConfig() {
